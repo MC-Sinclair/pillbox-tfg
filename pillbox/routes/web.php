0935c62d1cb591348fd17e99cba75eb9d95a52fd
Daito-2 <marcosg.cacsin@educa.jcyl.es>
2026-05-02 15:53:13 +0200

    feat/ añadir rutas de la landing

--- pillbox/routes/web.php
+++ pillbox/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Medico;
 use App\Http\Controllers\PanelController;
 use Illuminate\Support\Facades\Route;
 
+Route::inertia('/', 'Landing')->name('landing');
+
 Route::middleware(['auth', 'role:gerocultora'])->group(function () {
     Route::get('/panel', [PanelController::class, 'index'])->name('panel.index');
 });
