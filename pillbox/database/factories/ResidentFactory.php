81d0da7dd289af29a97e853d6e938af73cb475de
Daito-2 <marcosg.cacsin@educa.jcyl.es>
2026-05-02 15:07:59 +0200

    fix/añadir HasFactory a Resident y corregir namespace de fake() en ResidentFactory

--- pillbox/database/factories/ResidentFactory.php
+++ pillbox/database/factories/ResidentFactory.php
@@ -10,12 +10,12 @@ class ResidentFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name'   => $this->faker->firstName(),
-            'last_name'    => $this->faker->lastName() . ' ' . $this->faker->lastName(),
-            'birth_date'   => $this->faker->dateTimeBetween('-95 years', '-65 years')->format('Y-m-d'),
-            'room'         => (string) $this->faker->numberBetween(1, 50),
+            'first_name'   => \fake()->firstName(),
+            'last_name'    => \fake()->lastName() . ' ' . \fake()->lastName(),
+            'birth_date'   => \fake()->dateTimeBetween('-95 years', '-65 years')->format('Y-m-d'),
+            'room'         => (string) \fake()->numberBetween(1, 50),
             'photo'        => null,
-            'doctor'       => 'Dr. ' . $this->faker->lastName(),
+            'doctor'       => 'Dr. ' . \fake()->lastName(),
             'status'       => 'active',
             'residence_id' => Residence::factory(),
         ];
