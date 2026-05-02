bb4f249a4220dc590cc436d25f254b0267887ca4
Daito-2 <marcosg.cacsin@educa.jcyl.es>
2026-05-02 15:43:08 +0200

    feat/añadir middleware EnsureRole y registrar alias en bootstrap

--- pillbox/bootstrap/app.php
+++ pillbox/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureRole;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
@@ -14,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias(['role' => EnsureRole::class]);
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
