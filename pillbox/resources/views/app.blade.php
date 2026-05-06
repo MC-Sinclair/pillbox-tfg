a405d2ce47fe7dfb7ae28aaf20bf2cab28e0167b
Daito-2 <marcosg.cacsin@educa.jcyl.es>
2026-05-07 00:16:46 +0200

    fix/ corregir app.blade.php con directivas Inertia y Vite

--- pillbox/resources/views/app.blade.php
+++ pillbox/resources/views/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h2>PillBox llega pronto...</h2>
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+        @inertiaHead
+    </head>
+    <body>
+        @inertia
+    </body>
 </html>
