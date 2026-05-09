98a7cbb0da776040be6c1bf4a0d051fcb9dcf55b
Daito-2 <marcosg.cacsin@educa.jcyl.es>
2026-05-09 17:25:06 +0200

    feat/añadir favicon y título PillBox

--- pillbox/resources/views/app.blade.php
+++ pillbox/resources/views/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="/PillBoxLogo.png">
+        <title>PillBox</title>
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         @inertiaHead
